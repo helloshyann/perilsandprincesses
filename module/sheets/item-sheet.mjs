d0074e0dd63d57e758411340a67d427ef90ed5ef
helloshyann <71602314+helloshyann@users.noreply.github.com>
2026-01-03 15:31:18 -0500

    Cleaning Up

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -1,93 +1,110 @@
 import {
-  onManageActiveEffect,
-  prepareActiveEffectCategories,
-} from '../helpers/effects.mjs';
+	onManageActiveEffect,
+	prepareActiveEffectCategories,
+} from "../helpers/effects.mjs";
 
 /**
  * Extend the basic ItemSheet with some very simple modifications
  * @extends {ItemSheet}
  */
 export class PerilsAndPrincessesItemSheet extends ItemSheet {
-  /** @override */
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ['perilsandprincesses', 'sheet', 'item'],
-      width: 520,
-      height: 480,
-      tabs: [
-        {
-          navSelector: '.sheet-tabs',
-          contentSelector: '.sheet-body',
-          initial: 'description',
-        },
-      ],
-    });
-  }
+	/** @override */
+	static get defaultOptions() {
+		return foundry.utils.mergeObject(super.defaultOptions, {
+			classes: ["perilsandprincesses", "sheet", "item"],
+			width: 520,
+			height: 480,
+			tabs: [
+				{
+					navSelector: ".sheet-tabs",
+					contentSelector: ".sheet-body",
+					initial: "description",
+				},
+			],
+		});
+	}
 
-  /** @override */
-  get template() {
-    const path = 'systems/perilsandprincesses/templates/item';
-    // Return a single sheet for all item types.
-    // return `${path}/item-sheet.hbs`;
+	/** @override */
+	get template() {
+		const path = "systems/perilsandprincesses/templates/item";
+		// Return a single sheet for all item types.
+		// return `${path}/item-sheet.hbs`;
 
-    // Alternatively, you could use the following return statement to do a
-    // unique item sheet by type, like `weapon-sheet.hbs`.
-    return `${path}/item-${this.item.type}-sheet.hbs`;
-  }
+		// Alternatively, you could use the following return statement to do a
+		// unique item sheet by type, like `weapon-sheet.hbs`.
+		return `${path}/item-${this.item.type}-sheet.hbs`;
+	}
 
-  /* -------------------------------------------- */
+	/* -------------------------------------------- */
 
-  /** @override */
-  async getData() {
-    // Retrieve base data structure.
-    const context = super.getData();
+	/** @override */
+	async getData() {
+		// Retrieve base data structure.
+		const context = super.getData();
 
-    // Use a safe clone of the item data for further operations.
-    const itemData = this.document.toObject(false);
+		// Use a safe clone of the item data for further operations.
+		const itemData = this.document.toObject(false);
 
-    // Enrich description info for display
-    // Enrichment turns text like `[[/r 1d20]]` into buttons
-    context.enrichedDescription = await TextEditor.enrichHTML(
-      this.item.system.description,
-      {
-        // Whether to show secret blocks in the finished html
-        secrets: this.document.isOwner,
-        // Necessary in v11, can be removed in v12
-        async: true,
-        // Data to fill in for inline rolls
-        rollData: this.item.getRollData(),
-        // Relative UUID resolution
-        relativeTo: this.item,
-      }
-    );
+		// Enrich description info for display
+		// Enrichment turns text like `[[/r 1d20]]` into buttons
+		context.enrichedDescription = await TextEditor.enrichHTML(
+			this.item.system.description,
+			{
+				// Whether to show secret blocks in the finished html
+				secrets: this.document.isOwner,
+				// Necessary in v11, can be removed in v12
+				async: true,
+				// Data to fill in for inline rolls
+				rollData: this.item.getRollData(),
+				// Relative UUID resolution
+				relativeTo: this.item,
+			}
+		);
 
-    // Add the item's data to context.data for easier access, as well as flags.
-    context.system = itemData.system;
-    context.flags = itemData.flags;
+		// Add the item's data to context.data for easier access, as well as flags.
+		context.system = itemData.system;
+		context.flags = itemData.flags;
 
-    // Adding a pointer to CONFIG.PERILSANDPRINCESSES
-    context.config = CONFIG.PERILSANDPRINCESSES;
+		// Adding a pointer to CONFIG.PERILSANDPRINCESSES
+		context.config = CONFIG.PERILSANDPRINCESSES;
 
-    // Prepare active effects for easier access
-    context.effects = prepareActiveEffectCategories(this.item.effects);
+		// Prepare active effects for easier access
+		context.effects = prepareActiveEffectCategories(this.item.effects);
 
-    return context;
-  }
+		return context;
+	}
 
-  /* -------------------------------------------- */
+	/* -------------------------------------------- */
 
-  /** @override */
-  activateListeners(html) {
-    super.activateListeners(html);
+	/** @override */
+	activateListeners(html) {
+		super.activateListeners(html);
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.isEditable) return;
+		// Everything below here is only needed if the sheet is editable
+		if (!this.isEditable) return;
 
-    // Roll handlers, click handlers, etc. would go here.
+		// Roll handlers, click handlers, etc. would go here.
 
-    // Active Effect management
-    html.on('click', '.effect-control', (ev) =>
-      onManageActiveEffect(ev, this.item)
-    );
-  }
+		// Active Effect management
+		html.on("click", ".effect-control", (ev) =>
+			onManageActiveEffect(ev, this.item)
+		);
+
+		html.find(".pp-item-roll").click((ev) => {
+			const item = this.item;
+			const num = item.system.roll.diceNum || 1;
+			const size = item.system.roll.diceSize || "d6";
+			const bonus = item.system.roll.diceBonus
+				? ` + ${item.system.roll.diceBonus}`
+				: "";
+
+			const formula = `${num}${size}${bonus}`;
+			const roll = new Roll(formula);
+
+			roll.toMessage({
+				speaker: ChatMessage.getSpeaker({ item: this.item }),
+				flavor: `Using ${item.name}`,
+			});
+		});
+	}
 }
